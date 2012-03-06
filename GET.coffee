5d167d93893525f845726913fe51c14acd9a934c
m-r-r <raybaudroigm@gmail.com>
2012-03-06 13:55:40 +0100

    Rewrited in Coffee-Script

--- GET.coffee
+++ GET.coffee
@@ -5,18 +5,16 @@ Licensed under the BSD 3-clause license, see the COPYING file for details
 ###
 
 parse_query = (url) ->
-    unless url?
-        return
-    query = ( url.split('?', 2) )[1]
-    unless query?
-        return
-    args = query.split('&')
     decoded = {}
+    return unless url?
+    query = ( url.split('?', 2) )[1]
+    return unless query?
+    args = query.split('&')
 
     for arg in args
-        [key, value] = arg.split('?', 2)
-        if key?
-            decoded[(key).lower()] = value
+        [key, value] = arg.split('=', 2)
+        if key? and key.length > 0
+            decoded[key.toLowerCase()] = value
 
     return decoded
 
--- GET.js
+++ GET.js
@@ -1,27 +1,34 @@
-/*
-  GET.js v0.1
-  Copyright (c) 2012, Mickaël Raybaud-Roig
-  All rights reserved.
-  Licensed under the BSD 3-clause license, see the COPYING file for details.
-//*/
 
-function parseQuery(url) {
-    "use strict";
-    var query = url.split("?", 2)[1],
-        result = {},
-        params = query.split("&"),
-        i = 0;
+/* 
+GET.js v0.2
+Copyright © 2012 Mickaël Raybaud-Roig, All rights reserved.
+Licensed under the BSD 3-clause license, see the COPYING file for details
+*/
 
-    for (i = 0; i < params.length; i++) {
-        var param = params[i].split("=", 2);
-        result[param[0].toLowerCase()] = param[1];
+(function() {
+  var parse_query;
+
+  parse_query = function(url) {
+    var arg, args, decoded, key, query, value, _i, _len, _ref;
+    decoded = {};
+    if (url == null) return;
+    query = (url.split('?', 2))[1];
+    if (query == null) return;
+    args = query.split('&');
+    for (_i = 0, _len = args.length; _i < _len; _i++) {
+      arg = args[_i];
+      _ref = arg.split('=', 2), key = _ref[0], value = _ref[1];
+      if ((key != null) && key.length > 0) decoded[key.toLowerCase()] = value;
     }
+    return decoded;
+  };
 
-    return result;
-}
+  if ((typeof window !== "undefined" && window !== null) && (typeof document !== "undefined" && document !== null)) {
+    window.$_GET = parse_query(document.location.href);
+  } else if (typeof module !== "undefined" && module !== null) {
+    module.exports = {
+      'parse_query': parse_query
+    };
+  }
 
-(function (url) {
-    "use strict";
-    window.$_GET = parseQuery(url);
-})(document.location.search);
-
+}).call(this);
